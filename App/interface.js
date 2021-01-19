fcdcc86ce344c30278113856a9600afe958a7605
dm-devtech <70208036+dm-devtech@users.noreply.github.com>
2021-01-19 12:35:59 +0000

    added index page instructions

--- App/interface.js
+++ App/interface.js
@@ -1,12 +1,3 @@
-Instructions
-
-- Create a new instance of the Scorecard class (e.g. myScore = new Scorecard)
-- To add rolls to the scorecard use the addRoll(FrameRoll, pinsKnockedDown) function. E.g. A player knocks down 5 pins in frame 1 roll 1 enter myScore.addRoll(1.1, 5) You cannot enter more than one turn at a time.
-- Use the total(x) function to output your score with x being the frame you want the score for.
-  E.g. If you want the total of frame 5 enter total(5)
-- To print the entire scorecard use print(y) where y is the frame you want the scorecard up to
-- To reset the scorecard to zero all entries use resetScorecard()
-
 
 // $(document).ready(function() {
 //   scorecard = new Scorecard();
